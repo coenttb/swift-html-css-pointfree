c11301228ea003caba356fa4931d2740fbf373d6
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-04-22 12:42:22 +0200

    wip

--- Sources/HTML+CSS/InlineStyle/LineHeight.swift
+++ Sources/HTML+CSS/InlineStyle/LineHeight.swift
@@ -20,3 +20,25 @@ extension HTML {
         self.inlineStyle(lineHeight, media: media, pre: pre, pseudo: pseudo)
     }
 }
+
+extension HTML {
+    @discardableResult
+    public func lineHeight<T: BinaryInteger>(
+        _ lineHeight: T?,
+        media: CSSAtRuleTypes.Media? = nil,
+        pre: String? = nil,
+        pseudo: Pseudo? = nil
+    ) -> HTMLInlineStyle<Self> {
+        self.inlineStyle(lineHeight.map { LineHeight(integerLiteral: Int($0)) }, media: media, pre: pre, pseudo: pseudo)
+    }
+
+    @discardableResult
+    public func lineHeight<T: BinaryFloatingPoint>(
+        _ lineHeight: T?,
+        media: CSSAtRuleTypes.Media? = nil,
+        pre: String? = nil,
+        pseudo: Pseudo? = nil
+    ) -> HTMLInlineStyle<Self> {
+        self.inlineStyle(lineHeight.map { LineHeight(floatLiteral: Double($0)) }, media: media, pre: pre, pseudo: pseudo)
+    }
+}
