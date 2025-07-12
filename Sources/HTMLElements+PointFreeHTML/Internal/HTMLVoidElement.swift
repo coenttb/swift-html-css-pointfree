d7cf60f4ef48ea87b63b86de1bb1d471bd870250
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-12 10:15:14 +0200

    move HTMLVoidElement protocol to swift-html-types.

--- Sources/HTMLElements+PointFreeHTML/Internal/HTMLVoidElement.swift
+++ Sources/HTMLElements+PointFreeHTML/Internal/HTMLVoidElement.swift
@@ -9,26 +9,9 @@ import Foundation
 import PointFreeHTML
 import HTMLElementTypes
 
-public protocol HTMLVoidElement: PointFreeHTML.HTML {}
-
-extension HTMLVoidElement where Self: Element {
-    public func callAsFunction() -> HTMLElement<HTMLEmpty> {
+extension HTMLVoidElement where Self: HTMLElementTypes.HTMLElement {
+    public func callAsFunction() -> PointFreeHTML.HTMLElement<HTMLEmpty> {
         .init(tag: Self.tag) { HTMLEmpty() }
     }
 }
 
-//extension area: HTMLVoidElement {}
-//extension base: HTMLVoidElement {}
-
-
-//extension col: HTMLVoidElement {}
-//extension embed: HTMLVoidElement {}
-//extension hr: HTMLVoidElement {}
-//extension img: HTMLVoidElement {}
-//extension input: HTMLVoidElement {}
-//extension link: HTMLVoidElement {}
-//extension meta: HTMLVoidElement {}
-//extension param: HTMLVoidElement {}
-//extension source: HTMLVoidElement {}
-//extension track: HTMLVoidElement {}
-//extension wbr: HTMLVoidElement {}
